08a140d1eb85a6328e1c4d7d75f67c9e0f4af8b9
Vesna Lozanovic <vesna.lozanovic@gmail.com>
2020-05-03 16:03:20 +0100

    Search activity UI

--- app/src/main/java/com/vesna/stackexchange/presentation/search/SearchActivity.kt
+++ app/src/main/java/com/vesna/stackexchange/presentation/search/SearchActivity.kt
@@ -1,18 +1,30 @@
 package com.vesna.stackexchange.presentation.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.vesna.stackexchange.R
 import com.vesna.stackexchange.presentation.App
 import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.android.synthetic.main.view_holder.*
+import kotlinx.android.synthetic.main.view_holder.view.*
 import javax.inject.Inject
 
 class SearchActivity : AppCompatActivity() {
 
     @Inject
     lateinit var providerFactory: SearchProviderFactory
-    lateinit var viewModel: SearchViewModel
+    private lateinit var viewModel: SearchViewModel
+    private lateinit var adapter: ListAdapter<UserUiModel, ViewHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +32,65 @@ class SearchActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_search)
 
-        viewModel = ViewModelProvider(this, providerFactory)[SearchViewModel::class.java]
-
+        adapter = recyclerViewAdapter(recyclerView)
         button.setOnClickListener {
             viewModel.onSearchClicked()
         }
+        searchView.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                s?.toString().orEmpty().let {
+                    viewModel.onSearchTextChanged(it)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+
+        viewModel = ViewModelProvider(this, providerFactory)[SearchViewModel::class.java]
+
+        viewModel.uiStates.subscribe {
+            adapter.submitList(it.users)
+        }
 
 
+    }
+
+    private fun recyclerViewAdapter(recyclerView: RecyclerView): ListAdapter<UserUiModel, ViewHolder> {
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        adapter = object : ListAdapter<UserUiModel, ViewHolder>(Diff()) {
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.view_holder, parent, false)
+                return ViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                val item = getItem(position)
+                holder.itemView.reputation.text = item.reputation.toString()
+                holder.itemView.name.text = item.username
+                holder.itemView.setOnClickListener { viewModel.onUserClicked(item.userId) }
+            }
+        }
+        recyclerView.adapter = adapter
+        return adapter
+    }
+
+    private class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private class Diff : DiffUtil.ItemCallback<UserUiModel>() {
+
+        override fun areItemsTheSame(oldItem: UserUiModel, newItem: UserUiModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: UserUiModel, newItem: UserUiModel): Boolean {
+            return oldItem == newItem
+        }
     }
 }
