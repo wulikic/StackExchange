2acada7344f15d9611c75e9df126b855074a778c
Vesna Lozanovic <vesna.lozanovic@gmail.com>
2020-05-03 16:01:17 +0100

    Fix retrofit query

--- app/src/main/java/com/vesna/stackexchange/data/retrofit/RetrofitApi.kt
+++ app/src/main/java/com/vesna/stackexchange/data/retrofit/RetrofitApi.kt
@@ -2,18 +2,17 @@ package com.vesna.stackexchange.data.retrofit
 
 import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
-//https://api.stackexchange.com/2.2/users?page=1&pagesize=20&order=asc&sort=name&inname=john&site=stackoverflow
 interface RetrofitApi {
 
-    @GET("2.2/users?page={page}&pagesize={pagesize}&order={order}&sort={sort}&inname={inname}&site={site}")
+    @GET("2.2/users")
     fun search(
-        @Path("page") page: Int,
-        @Path("pagesize") pageSize: Int,
-        @Path("order") order: String,
-        @Path("sort") sort: String,
-        @Path("inname") nameQuery: String,
-        @Path("site") site: String
+        @Query("page") page: Int,
+        @Query("pagesize") pageSize: Int,
+        @Query("order") order: String,
+        @Query("sort") sort: String,
+        @Query("inname") nameQuery: String,
+        @Query("site") site: String
     ): Single<UserListResponse>
 }
